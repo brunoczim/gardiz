6386357e02fb6cdc0a68a2611d5cbd48ec53bce2
brunoczim <brunoczim@users.noreply.github.com>
2021-08-05 14:55:07 +0000

    deploy: 3dac36d758986b8f066f15f120caf45fc5670a73

--- gardiz/graph/sidebar-items.js
+++ gardiz/graph/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Components","Iterator over connected components of the graph. See [`Graph::components`]."],["Graph","A graph of points in a plane."]],"type":[["VertexEdges","The edges of a vertex. More specifically, at which direction the vertex is connected?"]]});
+initSidebarItems({"struct":[["Components","Iterator over connected components of the graph. See [`Graph::components`]."],["Graph","A graph of points in a plane."],["PathMakerBuf",""]],"type":[["VertexEdges","The edges of a vertex. More specifically, at which direction the vertex is connected?"]]});
