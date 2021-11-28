044ad3a94c358f3e725ab309b4a2b08bd8d2d9c9
brunoczim <brunoczim@users.noreply.github.com>
2021-11-28 01:07:00 +0000

    deploy: d867977aa7d755650428848d2af18557b0bb9a86

--- crates.js
+++ crates.js
@@ -1,1 +1,1 @@
-window.ALL_CRATES = ["gardiz","hashbrown","indexmap","num","num_bigint","num_complex","num_integer","num_iter","num_rational","num_traits","priority_queue","proc_macro2","quote","serde","serde_derive","syn","unicode_xid"];
+window.ALL_CRATES = ["gardiz","num","num_bigint","num_complex","num_integer","num_iter","num_rational","num_traits","proc_macro2","quote","serde","serde_derive","syn","unicode_xid"];
--- source-files.js
+++ source-files.js
@@ -1,7 +1,5 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["gardiz"] = {"name":"","files":["axis.rs","bits.rs","coord.rs","direc.rs","graph.rs","lib.rs","map.rs","rect.rs","set.rs"]};
-sourcesIndex["hashbrown"] = {"name":"","dirs":[{"name":"external_trait_impls","files":["mod.rs"]},{"name":"raw","files":["alloc.rs","bitmask.rs","mod.rs","sse2.rs"]}],"files":["lib.rs","macros.rs","map.rs","scopeguard.rs","set.rs"]};
-sourcesIndex["indexmap"] = {"name":"","dirs":[{"name":"map","dirs":[{"name":"core","files":["raw.rs"]}],"files":["core.rs"]}],"files":["equivalent.rs","lib.rs","macros.rs","map.rs","mutable_keys.rs","set.rs","util.rs"]};
 sourcesIndex["num"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["num_bigint"] = {"name":"","dirs":[{"name":"bigint","files":["addition.rs","bits.rs","convert.rs","division.rs","multiplication.rs","power.rs","shift.rs","subtraction.rs"]},{"name":"biguint","files":["addition.rs","bits.rs","convert.rs","division.rs","iter.rs","monty.rs","multiplication.rs","power.rs","shift.rs","subtraction.rs"]}],"files":["bigint.rs","biguint.rs","lib.rs","macros.rs"]};
 sourcesIndex["num_complex"] = {"name":"","files":["cast.rs","lib.rs","pow.rs"]};
@@ -9,7 +7,6 @@ sourcesIndex["num_integer"] = {"name":"","files":["average.rs","lib.rs","roots.r
 sourcesIndex["num_iter"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["num_rational"] = {"name":"","files":["lib.rs","pow.rs"]};
 sourcesIndex["num_traits"] = {"name":"","dirs":[{"name":"ops","files":["checked.rs","inv.rs","mod.rs","mul_add.rs","overflowing.rs","saturating.rs","wrapping.rs"]}],"files":["bounds.rs","cast.rs","float.rs","identities.rs","int.rs","lib.rs","macros.rs","pow.rs","real.rs","sign.rs"]};
-sourcesIndex["priority_queue"] = {"name":"","dirs":[{"name":"double_priority_queue","files":["iterators.rs","mod.rs"]},{"name":"priority_queue","files":["iterators.rs","mod.rs"]}],"files":["core_iterators.rs","lib.rs","store.rs"]};
 sourcesIndex["proc_macro2"] = {"name":"","files":["detection.rs","fallback.rs","lib.rs","marker.rs","parse.rs","wrapper.rs"]};
 sourcesIndex["quote"] = {"name":"","files":["ext.rs","format.rs","ident_fragment.rs","lib.rs","runtime.rs","spanned.rs","to_tokens.rs"]};
 sourcesIndex["serde"] = {"name":"","dirs":[{"name":"de","files":["ignored_any.rs","impls.rs","mod.rs","seed.rs","utf8.rs","value.rs"]},{"name":"private","files":["de.rs","doc.rs","mod.rs","ser.rs","size_hint.rs"]},{"name":"ser","files":["fmt.rs","impls.rs","impossible.rs","mod.rs"]}],"files":["integer128.rs","lib.rs","macros.rs"]};
